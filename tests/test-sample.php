18e69877cc574bab0e14c981d1c0efeb5e9f5dda
Olivier van Helden <gudule@speculoos.world>
2021-03-04 14:05:29 -0400

    Initial commit

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wppus_Woocommerce_Integration
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
---
+++ wppus-woocommerce-integration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     WP Plugin Update Server Woocommerce integration
+ * Plugin URI:      https://git.magiiic.com/wordpress/wppus-woocommerce-integration
+ * Description:     All said in the title
+ * Author:          Magiiic
+ * Author URI:      https://magiiic.com/
+ * Text Domain:     wppus-woocommerce-integration
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wppus_Woocommerce_Integration
+ */
+
+// Your code starts here.
